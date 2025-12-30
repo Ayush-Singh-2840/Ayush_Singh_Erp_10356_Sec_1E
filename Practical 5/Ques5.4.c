0870ac48ae7053816c2576bea2f89e46b5fa4cd1
Ayush Singh <ayushsingh02506@gmail.com>
2025-12-30 23:51:53 +0530

    Updated

---
+++ Practical 5/Ques5.4.c
@@ -0,0 +1,32 @@
+//Name Ayush Singh Erp 10356
+#include <stdio.h>
+
+int main() {
+    int a, b, choice;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("1.Add\n2.Subtract\n3.Multiply\n4.Divide\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            printf("Result = %d", a + b);
+            break;
+        case 2:
+            printf("Result = %d", a - b);
+            break;
+        case 3:
+            printf("Result = %d", a * b);
+            break;
+        case 4:
+            printf("Result = %d", a / b);
+            break;
+        default:
+            printf("Invalid choice");
+    }
+
+    return 0;
+}
